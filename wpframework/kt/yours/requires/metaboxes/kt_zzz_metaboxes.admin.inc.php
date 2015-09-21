2332f3d87bc7833bdd4e7ce797b9766834d4bab2
Martin Hlaváč <hlavacm@hotmail.com>
2015-09-21 16:38:35 +0200

    Přidání obecných pomocných obsahových shortcodů
    na základě Bootstrapu...

--- wpframework/kt/yours/requires/metaboxes/kt_zzz_metaboxes.admin.inc.php
+++ wpframework/kt/yours/requires/metaboxes/kt_zzz_metaboxes.admin.inc.php
@@ -12,3 +12,21 @@ KT_MetaBox::createMultiple(KT_ZZZ_Theme_Config::getAllGenericFieldset(), KT_WP_C
 
 KT_MetaBox::createMultiple(KT_ZZZ_Post_Config::getAllGenericFieldset(), KT_WP_POST_KEY, KT_MetaBox_Data_Type_Enum::POST_META);
 
+// --- content ------------------------
+
+foreach (array(KT_WP_POST_KEY, KT_WP_PAGE_KEY) as $postType) {
+    $pageShortcodesMetabox = KT_MetaBox::createCustom("kt-zzz-$postType-shortcodes-metabox", __("Obsahové zktratky", ZZZ_DOMAIN), $postType, "kt_zzz_content_shortcodes_metabox_callback", false);
+    $pageShortcodesMetabox->setContext(KT_MetaBox::CONTEXT_SIDE);
+    $pageShortcodesMetabox->setPriority(KT_MetaBox::PRIORITY_LOW);
+    $pageShortcodesMetabox->register();
+}
+
+function kt_zzz_content_shortcodes_metabox_callback() {
+    echo "<ol>";
+    echo "<li><b>[zzz_row_start]</b> - <i>začátek řádku</i></li>";
+    echo "<li><b>[zzz_column_start]</b> - <i>začátek sloupce</i></li>";
+    echo "<li><b>[zzz_column_end]</b> - <i>konec sloupce</i></li>";
+    echo "<li><b>[zzz_row_end]</b> - <i>konec řádku</i></li>";
+    echo "<li><b>[zzz_clearfix]</b> - <i>reset rendrování UI</i></li>";
+    echo "</ol>";
+}
---
+++ wpframework/kt/yours/requires/shortcodes/kt_zzz_content_shortcodes.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+add_shortcode("zzz_row_start", "kt_zzz_row_start_shortcode_callback");
+
+function kt_zzz_row_start_shortcode_callback($atts) {
+    return "<div class=\"row contentColumns\">";
+}
+
+add_shortcode("zzz_row_end", "kt_zzz_row_end_shortcode_callback");
+
+function kt_zzz_row_end_shortcode_callback($atts) {
+    return "</div>";
+}
+
+add_shortcode("zzz_column_start", "kt_zzz_column_start_shortcode_callback");
+
+function kt_zzz_column_start_shortcode_callback($atts) {
+    return "<div class=\"col-sm-6\">";
+}
+
+add_shortcode("zzz_column_end", "kt_zzz_column_end_shortcode_callback");
+
+function kt_zzz_column_end_shortcode_callback($atts) {
+    return "</div>";
+}
+
+add_shortcode("zzz_clearfix", "kt_zzz_clearfix_shortcode_callback");
+
+function kt_zzz_clearfix_shortcode_callback() {
+    return "<div class=\"clearfix\"></div>";
+}
