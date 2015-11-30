c08bb6c4ac8a4bc487de745446a2f9b74ebf6294
Martin Hlaváč <hlavacm@hotmail.com>
2015-11-30 13:26:37 +0100

    Přidání vzorového taxonomy layoutu
    ala category

---
+++ wpframework/taxonomies/taxonomy.php
@@ -0,0 +1,39 @@
+<?php
+$termPresenter = new KT_WP_Term_Base_Presenter();
+$termModel = $termPresenter->getModel();
+get_header();
+?>
+
+<main id="category" class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <header>
+                <h1><?php echo $termModel->getName(); ?></h1>
+                <h2 class="hidden-xs"><?php echo $termModel->getDescription(); ?></h2>
+            </header>
+            <?php if (have_posts()) { ?>
+                <div class="row">
+                    <?php
+                    while (have_posts()) : the_post();
+                        get_template_part("loops/loop", $post->post_type);
+                    endwhile;
+                    ?>
+                </div>
+                <div id="pagination" class="clearfix">
+                    <?php echo $termPresenter->getPaginationLinks(); ?>
+                </div>
+                <?php
+            } else {
+                ?> 
+                <div class="row">
+                    <p><?php _e("K dispozici nejsou žádné příspěvky...", ZZZ_DOMAIN); ?></p>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+</main>
+
+<?php
+get_footer();
