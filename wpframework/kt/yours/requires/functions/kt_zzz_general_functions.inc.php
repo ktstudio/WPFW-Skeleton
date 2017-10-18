437aa2ec49a10f96d0d27556cca25eb804e7169c
Martin Hlaváč <hlavacm@hotmail.com>
2017-10-18 10:05:39 +0200

    Nástřel počtu vlastních post typů na dashboardu

--- wpframework/kt/yours/requires/functions/kt_zzz_general_functions.inc.php
+++ wpframework/kt/yours/requires/functions/kt_zzz_general_functions.inc.php
@@ -24,3 +24,22 @@ function kt_wph_posts_per_page($query) {
         }
     }
 }
+
+// --- dashboard: glance ------------------------
+
+add_filter("dashboard_glance_items", "kt_zzz_dashboard_glance_items_filter");
+
+function kt_zzz_dashboard_glance_items_filter(array $elements)
+{
+    // references
+    $productsKey = KT_ZZZ_REFERENCE_KEY;
+    $productsCounts = wp_count_posts($productsKey, "readable");
+    $productsLabel = sprintf(__("%d referencí", "ZZZ_ADMIN_DOMAIN"), $productsCounts->publish);
+    $elements[] = "<a href=\"edit.php?post_type=$productsKey\">$productsLabel</a>";
+    // sliders
+    $listsKey = KT_ZZZ_SLIDER_KEY;
+    $listsCounts = wp_count_posts($listsKey, "readable");
+    $listsLabel = sprintf(__("%d sliderů", "ZZZ_ADMIN_DOMAIN"), $listsCounts->publish);
+    $elements[] = "<a href=\"edit.php?post_type=$listsKey\">$listsLabel</a>";
+    return $elements;
+}
